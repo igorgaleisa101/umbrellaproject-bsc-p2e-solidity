7e0c764515ab42755e72bde2617d49276cfab6f7
iorgaleisa101 <igorgaleisa101@gmail.com>
2021-10-19 22:48:02 +0900

    Update DatabaseSeeder - 1

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -37,7 +37,6 @@ class DatabaseSeeder extends Seeder
         ]);
 
 
-        \App\Models\User::factory(10)->create();
-        // \App\Models\Equipment::factory(100)->create();
+        // \App\Models\User::factory(10)->create();
     }
 }
