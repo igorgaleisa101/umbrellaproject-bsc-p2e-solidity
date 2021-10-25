737090b0ac97348a0302a595c692f16b6d07420d
iorgaleisa101 <igorgaleisa101@gmail.com>
2021-10-26 03:47:11 +0900

    Update Connector - 1

--- resources/js/utils/connectors.js
+++ resources/js/utils/connectors.js
@@ -4,7 +4,7 @@ import { InjectedConnector } from "@web3-react/injected-connector";
 export const injected = new InjectedConnector({
   // supportedChainIds: [1, 3, 4, 5, 42, 1337], // Change according to supported Network Ids
   // supportedChainIds: [97], // Change according to supported Network Ids
-  supportedChainIds: [97], // Change according to supported Network Ids
-  // supportedChainIds: [56], // Change according to supported Network Ids
+  // supportedChainIds: [97], // Change according to supported Network Ids
+  supportedChainIds: [56], // Change according to supported Network Ids
 });
 
